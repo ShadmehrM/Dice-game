dde7f015650041ebd2358257fffac842cd7fb98e
ShadmehrM <98378888+ShadmehrM@users.noreply.github.com>
2022-03-27 16:01:50 +0430

    upload 1.0

---
+++ Index.js
@@ -0,0 +1,27 @@
+var rndNumber1 = Math.floor(Math.random() * 6) + 1;
+var rndImg = "images/" + "dice" + rndNumber1 + ".png";
+
+var tossOne = document.getElementsByClassName("img")[0];
+
+tossOne.setAttribute("src", rndImg);
+
+//************************************************************************************************ */
+
+var rndNumber2 = Math.floor(Math.random() * 6) + 1;
+var rndImg2 = "images/" + "dice" + rndNumber2 + ".png";
+
+var tossTwo = document.getElementsByClassName("img")[1];
+tossTwo.setAttribute("src", rndImg2);
+
+//************************************************************************************************ */
+
+if (rndNumber1 > rndNumber2) {
+
+    document.querySelector("h1").innerHTML = "🎉Player one WINS!";
+} else if (rndNumber2 > rndNumber1) {
+
+    document.querySelector("h1").innerHTML = "🎉Player two WINS!";
+} else {
+
+    document.querySelector("h1").innerHTML = "🤝🏼Draw!";
+}
